12ab951666cd25d376e6a81aa965427b7898518e
jmischek-convivo <j.mischek@convivo.com>
2019-03-07 16:12:34 +0100

    added exports to global index, updated readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # svg-live-drawing
+
+[![NPM package][npm]][npm-url]
+
 Library for interactive SVG drawing applications
+
+## Installation
+
+```sh
+npm install svg-live-drawing --save
+```
 
 ## Scripts
 
@@ -14,3 +23,6 @@ Library for interactive SVG drawing applications
 #### Run unit tests with code coverage
 
     npm run test
+
+[npm]: https://img.shields.io/npm/v/svg-live-drawing.svg
+[npm-url]: https://www.npmjs.com/package/svg-live-drawing
--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,29 @@ export { SelectionManager } from './selection-manager';
 export { ToolManager } from './tool-manager';
 export { FloatBar } from './float-bar';
 export { SvgConstants } from './svg-constants';
+
+export { Context } from './svg-elements/context';
+export { Grid } from './svg-elements/grid';
+export { Group } from './svg-elements/group';
+export { Pattern } from './svg-elements/pattern';
+
+export { ControlPoint } from './control-objects/control-point';
+export { ControlObject } from './control-objects/control-object';
+export { PolylineControl } from './control-objects/polyline-control';
+export { PolygonControl } from './control-objects/polygon-control';
+export { RectangleControl } from './control-objects/rectangle-control';
+export { CircleControl } from './control-objects/circle-control';
+export { ThreePointCircleControl } from './control-objects/three-point-circle-control';
+export { ControlFactory } from './control-objects/control-factory';
+
+export { ControlModifier } from './control-modifiers/control-modifier';
+export { OrthogonalModifier } from './control-modifiers/orthogonal-modifier';
+export { GridModifier } from './control-modifiers/grid-modifier';
+
+export { BasicLine } from './basic-shapes/basic-line';
+export { BasicPolygon } from './basic-shapes/basic-polygon';
+export { BasicPolyline } from './basic-shapes/basic-polyline';
+export { BasicRectangle } from './basic-shapes/basic-rectangle';
+export { BasicCircle } from './basic-shapes/basic-circle';
+export { BasicText } from './basic-shapes/basic-text';
+export { BasicPath } from './basic-shapes/basic-path';
